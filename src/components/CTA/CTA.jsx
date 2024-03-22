1eedc417d3c6effa33d761577183eeb0c91d3f0e
Morisreddenton <publicpartnersinc@gmail.com>
2024-03-22 06:14:32 +0000

    Added fix to netlify redirects v1.05

--- src/components/CTA/CTA.jsx
+++ src/components/CTA/CTA.jsx
@@ -12,7 +12,7 @@ const CTA = () => {
       <RightContainer>
         <div className="cta-info">
           <h1>
-            Join <span>5000+</span> People Who Are <br /> Already Connected.
+            Join <span>5000+</span> People Who Are Already Connected.
           </h1>
           <div className="text">
             <p>
